ce95ef183964a2d53e32084c2d48f4108f91c8be
Nick-ch <nd.chernikov@gmail.com>
2018-01-24 13:44:06 +0300

    add calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,17 @@
+import makePair from '../pairs';
+import getRandom, { isEven } from '../tools';
+import runEngine from './engine/engine';
+
+const getCorrectAnswer = number => (isEven(number) ? 'yes' : 'no');
+
+const generateQuestion = () => {
+  const question = Math.round(getRandom(1, 100));
+  const answer = getCorrectAnswer(question);
+  return makePair(question, answer);
+};
+
+const runGame = () => {
+  runEngine(generateQuestion, 'What is the result of the expression?');
+};
+
+export default runGame;
