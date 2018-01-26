1898c45b1e78a386f02a9ec1d143d23ac208804a
cognitive-cake <nd.chernikov@gmail.com>
2018-01-26 18:04:02 +0300

    fix finding correct answer for progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,36 +2,45 @@ import makePair from '../pairs';
 import getRandom from '../tools';
 import runEngine from '..';
 
-let answer = null;
-
-const addMissingNum = (arr, currVal) => {
-  answer = currVal;
-  return arr.concat('..');
-};
-
-const createArray = (arr, step, currVal, missingNum, counter) => {
+const createArrayOfProgression = (arr, step, currVal, missingNum, counter) => {
   if (counter === 10) {
     return arr;
   }
   const newArray = counter === missingNum ?
-    addMissingNum(arr, currVal) :
+    arr.concat('..') :
     arr.concat(currVal);
 
-  return createArray(newArray, step, currVal + step, missingNum, counter + 1);
+  return createArrayOfProgression(newArray, step, currVal + step, missingNum, counter + 1);
 };
 
 const createArithmeticProgression = () => {
   const step = Math.round(getRandom(2, 10));
   const firstNum = Math.round(getRandom(2, 15));
   const missingNum = Math.round(getRandom(0, 9));
-  const array = createArray([], step, firstNum, missingNum, 0);
+  const array = createArrayOfProgression([], step, firstNum, missingNum, 0);
 
   return array.join(' ');
 };
 
+const getCorrectAnswer = (str) => {
+  const arr = str.split(' ');
+  const missingNumIndex = arr.findIndex(elem => elem === '..');
+
+  if (missingNumIndex === arr.length - 1 ||
+      missingNumIndex === arr.length - 2) {
+    const step = arr[1] - arr[0];
+    const missingNumValue = parseInt(arr[missingNumIndex - 1], 10) + step;
+    return missingNumValue;
+  }
+
+  const step = arr[missingNumIndex + 2] - arr[missingNumIndex + 1];
+  const missingNumValue = arr[missingNumIndex + 1] - step;
+  return missingNumValue;
+};
+
 const createTask = () => {
   const question = createArithmeticProgression();
-  const correctAnswer = answer;
+  const correctAnswer = getCorrectAnswer(question);
   return makePair(question, correctAnswer);
 };
 
